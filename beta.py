38f1dc419d48a4f44f24ff2c07d07415b8935d20
fuz <fuz.woo.cn@gmail.com>
2015-05-30 10:45:13 +0800

    beta plotting parameters

--- beta.py
+++ beta.py
@@ -3,17 +3,19 @@
 import math
 import matplotlib.pyplot as plot
 from numpy import *
+from scipy import special
 
 def beta(x,y):
+    return special.beta(x,y)
     return math.gamma(x)*math.gamma(y)/math.gamma(x+y)
 
 def betad(x,a,b):
     return (1.0/beta(a,b))*(x**(a-1))*((1-x)**(b-1))
 
-def plotbetad(a,b):
-    x = arange(0,1,0.01)
+def plotbetad(a,b,**params):
+    x = arange(0,1,0.001)
     y = betad(x,a,b)
-    plot.plot(x,y)
+    plot.plot(x,y,**params)
 
 def plot_with_sigma(sigma,c=1):
     a = (1/(1.0-sigma))**c
